c5b8f455e67f86c27ae427807c16ec5b53b77c31
Jumanah Colvin <jumanah.colvin@gmail.com>
2019-07-08 20:38:31 -0400

    remove blank line

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -2,7 +2,6 @@ const { paginateResults } = require('./utils');
 
 module.exports = {
   Query: {
-    
     launches: async (_, { pageSize = 20, after }, { dataSources }) => {
       const allLaunches = await dataSources.launchAPI.getAllLaunches();
       // we want these in reverse chronological order
